30d2fc6ca6a71a41d27dbaa11a37dc42911815f1
C_Jenkins <gbengachris87@gmail.com>
2026-01-17 18:41:22 +0100

    `Added icons and text to bottom tab component`

--- linkedIn/app/components/bottomtab.jsx
+++ linkedIn/app/components/bottomtab.jsx
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
-
+import { View, TouchableOpacity, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const BottomTab = () => {
   return (
-    <View>
-    <Text>dfhjdhfjdf</Text>
-    </View>)}
+    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+      <TouchableOpacity style={{alignItems: "center"}}><Ionicons name="home" size={30} color="black" /><Text style={{color: 'grey', fontSize: 10}}>Home</Text></TouchableOpacity>
+      <TouchableOpacity style={{alignItems: "center"}}><Ionicons name="people" size={30} color="grey" /><Text style={{color: 'grey', fontSize: 10}}>Network</Text></TouchableOpacity>
+      <TouchableOpacity style={{alignItems: "center"}}><Ionicons name="duplicate" size={30} color="grey" /><Text style={{color: 'grey', fontSize: 10}}>Post</Text></TouchableOpacity>
+      <TouchableOpacity style={{alignItems: "center"}}><Ionicons name="notifications" size={30} color="grey" /><Text style={{color: 'grey', fontSize: 10}}>Notifications</Text></TouchableOpacity>
+      <TouchableOpacity style={{alignItems: "center"}}><Ionicons name="briefcase" size={30} color="grey" /><Text style={{color: 'grey', fontSize: 10}}>Jobs</Text></TouchableOpacity>
+    </View>
+  );
+};
 
 export default BottomTab;
