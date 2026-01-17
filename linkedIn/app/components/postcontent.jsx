b8a4be8cc82ba3e87e599d74f16b06f43470ba5f
C_Jenkins <gbengachris87@gmail.com>
2026-01-17 18:58:01 +0100

    `Added readMore state and updated post content text`

--- linkedIn/app/components/postcontent.jsx
+++ linkedIn/app/components/postcontent.jsx
@@ -1,20 +1,23 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import PostHead from "./posthead";
+import { useState } from "react";
 
 const Postcontent = () => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <View>
       <PostHead />
 
-      <View
-      //   style={{ flexDirection: "row"}}
-      >
+      <View>
         <Text>
           This is the content of the post. Lorem ipsum dolor, sit amet
-          consectetur adipisicing elit. Suscipit eaque possimus est consequuntur
-          ad, magni perspiciatis iusto enim consequatur sapiente!  
+          consectetur adipisicing elit. Temporibus atque velit inventore! Et
+          voluptatem doloremque suscipit minus assumenda temporibus consequuntur
+          accusantium labore perferendis aspernatur? Culpa labore soluta nostrum
+          nulla eligendi distinctio velit corrupti, praesentium tempore omnis
+          assumenda architecto saepe aut.!
           <TouchableOpacity>
-            <Text style={{ color: "grey", fontWeight: 650}}>  ...more</Text>
+            <Text style={{ color: "grey", fontWeight: 650 }}> ...more</Text>
           </TouchableOpacity>
         </Text>
       </View>
@@ -23,3 +26,5 @@ const Postcontent = () => {
 };
 
 export default Postcontent;
+
+
