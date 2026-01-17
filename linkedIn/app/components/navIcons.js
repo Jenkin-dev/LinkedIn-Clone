d835bdce5cf4a5f45cf0582fd4221b6205ba6bab
C_Jenkins <gbengachris87@gmail.com>
2026-01-17 08:21:05 +0100

    Project Reset
    I resetted the project and also created a component to start woking which still remain uncompleted

---
+++ linkedIn/app/components/navIcons.js
@@ -0,0 +1,12 @@
+import { Ionicons } from "@expo/vector-icons";
+
+
+
+
+const Icon =({})  => {
+//     // return (
+        
+//     // )
+// }
+
+export default Icon;
---
+++ linkedIn/app/index.jsx
@@ -0,0 +1,14 @@
+import { Text, View, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <View></View>
+
+      <ScrollView></ScrollView>
+
+      <View></View>
+    </SafeAreaView>
+  );
+}
