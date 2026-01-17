421930c4a94264f56b9bad0ca3471f63fce249c9
C_Jenkins <gbengachris87@gmail.com>
2026-01-17 17:41:40 +0100

    `Updated post component: changed text color of "...more" button to grey`

--- linkedIn/app/components/post.jsx
+++ linkedIn/app/components/post.jsx
@@ -12,7 +12,7 @@ const Post = () => {
         <Text>
             This is the content of the post. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit eaque possimus est consequuntur ad, magni perspiciatis iusto enim consequatur sapiente!
         </Text>
-        <TouchableOpacity><Text>...more</Text></TouchableOpacity>
+        <TouchableOpacity><Text style={{color: 'grey'}}>...more</Text></TouchableOpacity>
       </View>
 
     </View>
