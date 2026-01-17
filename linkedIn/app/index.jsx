db4dcf1b0379bb18a78ec8fea405f6cbe563f73d
C_Jenkins <gbengachris87@gmail.com>
2026-01-17 08:56:59 +0100

    Started work on it

--- linkedIn/app/index.jsx
+++ linkedIn/app/index.jsx
@@ -1,13 +1,20 @@
-import { Text, View, ScrollView } from "react-native";
+import { Text, View, ScrollView, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <View></View>
+    <SafeAreaView
+      style={{ backgroundColor: "lightyellow", flex: 1, padding: 20 }}
+    >
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={{ flexDirection: "row" }}>
+          <Image style={{width: 30, height: 30}}source={require("../assets/images/WhatsApp Image 2.jpeg")} />
+        </View>
 
-      <ScrollView>
-        {/* <Text>Okay welcome my name is Oreofe-Olanrewaju Christopher</Text> */}
+        <Text>Okay welcome my name is Oreofe-Olanrewaju Christopher</Text>
       </ScrollView>
 
       <View></View>
